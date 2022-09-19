3bbd220b11d9afd2ef6ebe71b9d396f62c7cb1cf
Linuxcode1 <64568214+Linuxcode1@users.noreply.github.com>
2022-09-19 02:23:29 +0200

    Update #installerBlackPicons.sh

--- #installerBlackPicons.sh
+++ #installerBlackPicons.sh
@@ -29,10 +29,10 @@ if [ -f $MY_TMP_FILE ]; then
 	echo 'Extracting ...'
 	echo $MY_SEP
 	echo ''
-	#tar -xf $MY_TMP_FILE -C /
+	
 	MY_RESULT=$?
 
-	rm -f $MY_TMP_FILE > /dev/null 2>&1
+	
 
 
 	echo ''
